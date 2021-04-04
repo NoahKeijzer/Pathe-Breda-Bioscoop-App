5db4eedb40bbcc7e2933ae91c96597adbd0ec30f
Noah de Keijzer <noah.cristiaan@gmail.com>
2021-04-04 15:44:35 +0200

    Api Task Personal List
    Dit ja

--- app/src/main/java/com/example/pathebredabioscoopapp/logic/JSONConverter.java
+++ app/src/main/java/com/example/pathebredabioscoopapp/logic/JSONConverter.java
@@ -75,30 +75,6 @@ public class JSONConverter implements Serializable {
         return filmLists;
     }
 
-    public ArrayList<FilmList> convertFilmList() {
-        ArrayList<FilmList> filmLists = new ArrayList<>();
-
-        try {
-            JSONObject responseObject = new JSONObject(response);
-            JSONArray responseResultSet = responseObject.getJSONArray(delimterResults);
-
-            for (int s = 0; s < responseResultSet.length(); s++) {
-                JSONObject resultSetObject = responseResultSet.getJSONObject(s);
-                int id = resultSetObject.getInt(JSON_FILMLIST_ID);
-                String name = resultSetObject.getString(JSON_FILMLIST_NAME);
-                ArrayList<Films> filmList = new ArrayList<>();
-
-                filmLists.add(new FilmList(id, name, filmList));
-            }
-        } catch (JSONException e) {
-            Log.d(TAG, "Geen JSON object");
-            e.printStackTrace();
-        }
-        return filmLists;
-    }
-
-
-
     public ArrayList<Films> convertFilm() {
         Log.d(TAG, "convertJsonToArrayList is aangeroepen");
         ArrayList<Films> results = new ArrayList<>();
