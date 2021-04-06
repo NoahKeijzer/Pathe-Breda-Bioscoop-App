ee38520e4ca7aa6d2a3287d51930e079c7625926
SpagettBro <markgonzalez02@hotmail.com>
2021-04-06 11:22:06 +0200

    Review update
    Review adapter aangemaakt en reviewactivity aangepast.

---
+++ app/src/main/java/com/example/pathebredabioscoopapp/logic/ReviewAdapter.java
@@ -0,0 +1,108 @@
+package com.example.pathebredabioscoopapp.logic;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pathebredabioscoopapp.R;
+import com.example.pathebredabioscoopapp.domain.FilmList;
+import com.example.pathebredabioscoopapp.domain.Films;
+import com.example.pathebredabioscoopapp.domain.Reviews;
+import com.example.pathebredabioscoopapp.ui.AllListsActivity;
+import com.example.pathebredabioscoopapp.ui.DetailActivity;
+import com.example.pathebredabioscoopapp.ui.PersonalListActivity;
+import com.example.pathebredabioscoopapp.ui.ReviewActivity;
+import com.squareup.picasso.Picasso;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> implements Serializable {
+    private final String TAG = getClass().getSimpleName();
+    private List<Reviews> reviewList;
+    private AllListsActivity allListsActivity = new AllListsActivity();
+
+    public ReviewAdapter(List reviewList) {
+        Log.d(TAG, "ReviewAdapter constructor is aangeroepen.");
+        this.reviewList = reviewList;
+    }
+
+    @NonNull
+    @Override
+    public ReviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder() is aangeroepen.");
+
+        Context context = parent.getContext();
+
+        int layoutIdForListItem = R.layout.review_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new ReviewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewAdapter.ReviewViewHolder holder, int position) {
+        Log.d(TAG, "onBind is aangeroepen");
+
+        Reviews review = reviewList.get(position);
+
+        holder.mUsername.setText(String.valueOf(review.getUsername()));
+        holder.mContent.setText(String.valueOf(review.getContent()));
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if (null == reviewList) {
+            Log.d(TAG, "getItemCount(): Er zijn 0 items.");
+            return 0;
+        }
+
+        Log.d(TAG, "getItemCount(): Er zijn " + reviewList.size() + " items.");
+        return reviewList.size();
+    }
+
+    public class ReviewViewHolder extends RecyclerView.ViewHolder {
+        private TextView mUsername;
+        private TextView mContent;
+
+        public ReviewViewHolder(@NonNull View view) {
+            super(view);
+
+            Log.d(TAG, "ViewHolder constructor is aangeroepen.");
+
+            mUsername = (TextView) itemView.findViewById(R.id.tv_username);
+            mContent = (TextView) itemView.findViewById(R.id.tv_review_description);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick() van een view is aangeroepen.");
+                    int position = 1;
+                    for (Reviews review : reviewList) {
+                        if (mUsername.getText().toString().equals(review.getUsername())) {
+                            position = reviewList.indexOf(review);
+                        }
+                    }
+
+                    Reviews review = reviewList.get(position);
+                    Context context = v.getContext();
+                    Class destinationActivity = ReviewActivity.class;
+                    Intent startChildActivityIntent = new Intent(context, destinationActivity);
+                    startChildActivityIntent.putExtra("REVIEW_NAME",  review);
+                    context.startActivity(startChildActivityIntent);
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/pathebredabioscoopapp/ui/ReviewActivity.java
+++ app/src/main/java/com/example/pathebredabioscoopapp/ui/ReviewActivity.java
@@ -1,28 +1,48 @@
 package com.example.pathebredabioscoopapp.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pathebredabioscoopapp.R;
+import com.example.pathebredabioscoopapp.domain.Actors;
+import com.example.pathebredabioscoopapp.domain.Films;
 import com.example.pathebredabioscoopapp.domain.Reviews;
+import com.example.pathebredabioscoopapp.logic.FilmAdapter;
+import com.example.pathebredabioscoopapp.logic.ReviewAdapter;
 import com.example.pathebredabioscoopapp.logic.ReviewsAPITask;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class ReviewActivity extends AppCompatActivity implements ReviewsAPITask.ReviewsListener {
     private ArrayList<Reviews> reviews;
+    private TextView mUsername;
+    private TextView mContent;
+    private ReviewAdapter reviewAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_recycler_view);
+        Reviews review = (Reviews) getIntent().getSerializableExtra("REVIEW_NAME");
+        fillViews(review);
 
         new ReviewsAPITask(this::onReviewAvailable).execute();
+    }
+
+    public void fillViews(Reviews review){
+        mUsername = findViewById(R.id.tv_username);
+        mUsername.setText(review.getUsername());
+        mContent = findViewById(R.id.tv_review_description);
+        mContent.setText(review.getContent());
     }
 
     @Override
     public void onReviewAvailable(ArrayList<Reviews> reviewsList) {
         this.reviews.addAll(reviewsList);
+        this.reviewAdapter.notifyDataSetChanged();
     }
 }
